e9c497e02a3721e2ca9e0c684ca0e8c27d420fb8
salim104 <salimshaban885@gmail.com>
2022-03-24 21:13:17 +0200

    Portfolio

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { projects } from '../../constants/constants';
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider/>
+    <SectionTitle main >Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((project) => {
+        return <div>
+               <h2>{project.title}</h2>
+               <p>{project.description}</p>
+        </div>
+      })}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
