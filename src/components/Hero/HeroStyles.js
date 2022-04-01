9852345660cee7d304407015172785f16402ae24
salim104 <salimshaban885@gmail.com>
2022-04-01 21:03:29 +0200

    added

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const LeftSection = styled.div`
   width: 100%;
   @media ${(props) => props.theme.breakpoints.sm} {
-    width: 80%;
+    width: 81%;
     display: flex;
     flex-direction: column;
 
